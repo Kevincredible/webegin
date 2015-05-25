6e72855d15b3c7798ac1c74ff12d9e5b599156f9
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-05-24 19:42:07 -0500

    Edited ternary operator

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta name="robots" content="noindex, nofollow">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?= $pageTitle ? 'Site Title' : 'Site Title | ' . $pageTitle ?></title>
+<title><?= $pageTitle ? 'Site Title | ' . $pageTitle : 'Site Title' ?></title>
 <link rel="stylesheet" type="text/css" href="css/main.min.css">
 <!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
