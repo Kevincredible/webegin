3cf4a5c875eb7482ba13a9a0db9f23b8bbd4e491
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-05-24 20:23:29 -0500

    Edited ternary operator

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
+<?php $siteTitle = 'Site Title'; ?>
 <!doctype html>
 <html>
 <head>
 <meta name="robots" content="noindex, nofollow">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?= $pageTitle ? 'Site Title | ' . $pageTitle : 'Site Title' ?></title>
+<title><?= $pageTitle ? $siteTitle . ' | ' . $pageTitle : $siteTitle ?></title>
 <link rel="stylesheet" type="text/css" href="css/main.min.css">
 <!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
