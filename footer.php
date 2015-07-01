f56ad123deb1de0ad86d637569a54c6fa5f56170
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-07-01 17:21:48 -0500

    Added Velocity.js

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/velocity/1.2.2/velocity.min.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?v=3.19&sensor=false"></script>
         <script src="js/viewport-units-buggyfill.min.js"></script>
         <script src="js/matchMedia.min.js"></script>
