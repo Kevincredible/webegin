9f70fcaa7660d8fc76ebd569fd742b0decdf0b02
Kevin Beronilla <kevin.beronilla@gmail.com>
2016-03-26 13:24:08 -0500

    Updated FontAwesome to 4.5.0

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ function isCurrent($target) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $pageTitle ? $siteTitle . ' | ' . $pageTitle : $siteTitle ?></title>
-        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="css/main.min.css">
     </head>
     <body>
