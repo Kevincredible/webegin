86f97221b71eb4e950c69ad1af0a3c69c493e70f
Kevin Beronilla <kevin.beronilla@gmail.com>
2016-04-19 09:23:00 -0500

    Added isDev variable

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
 $siteTitle = 'Site Title';
+$isDev = true;
 
 function isCurrent($target) {
     (strpos($_SERVER['PHP_SELF'], $target)) ?  $current = true :  $current = false;
@@ -9,11 +10,13 @@ function isCurrent($target) {
 <!doctype html>
 <html>
     <head>
+        <?php if (isDev) { ?>
         <meta name="robots" content="noindex, nofollow">
+        <?php } ?>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $pageTitle ? $siteTitle . ' | ' . $pageTitle : $siteTitle ?></title>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="css/main.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main<?= ($isDev) ? '' : '.min' ?>.css">
     </head>
     <body>
